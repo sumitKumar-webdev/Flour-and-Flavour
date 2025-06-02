8350b3bf7f3d1c8b6b6f04989ebfedc983692dbf
Sumit Kumar <sumitkr8178@gmail.com>
2025-06-03 00:36:39 +0530

    fixed syncing bug

--- src/Redux Slices/cartSlice.js
+++ src/Redux Slices/cartSlice.js
@@ -3,76 +3,86 @@ import Service from "../Appwrite/Config";
 import { cartDatafromLS } from "./LocalStorage";
 
 const initialState = cartDatafromLS() ?? {
-     products : [],
-     totalPrice : 0,
-     totalProducts : 0,
-     placeOrder: [],
-     message: null
+    products: [],
+    totalPrice: 0,
+    totalProducts: 0,
+    placeOrder: [],
+    message: null
 }
 
 export const fetchCartFromDb = createAsyncThunk(
     'cart/fetchCartFromDb',
-  
-        async (userId, {rejectWithValue}) => {
-            try {
-                const response = await Service.getAllCartProduct(userId);
-                return response.documents
-            } catch (error) {
-                console.log("fetchCart :: cartSlice :: error", error);
-                return rejectWithValue(error)
-            }
 
+    async (userId, { rejectWithValue }) => {
+        try {
+            const response = await Service.getAllCartProduct(userId);
+            return response.documents
+        } catch (error) {
+            console.log("fetchCart :: cartSlice :: error", error);
+            return rejectWithValue(error)
         }
+
+    }
 )
 
 export const syncCartToDb = createAsyncThunk(
     'cart/syncCartToDb',
-    async (_, {getState}) => {
-        const { products } = getState().cart
-        console.log(products);
-        
-        const userId = getState().auth.userData.$id;
-        if (!userId || products.length === 0) return;
-        await Promise.all(
-            products.map(async(item) =>{    
-                if (item.$id) {
-                    const updatingItem = {
-                        documentId: item.$id,
-                        userId : userId,
-                        productId: item.productId,
-                        price: item.price,
-                        quantity: item.quantity,
-                        size: item.size,
+    async (_, { getState }) => {
+        try {
+            const { products } = getState().cart
+            const userId = getState().auth.userData?.$id;
+
+            if (!userId || products.length === 0) return;
+
+            const syncTasks = products.map(async (product) => {
+                try {
+                    const productId = product.productId;
+                    const size = product.size;
+                    const response = await Service.getExistingCartProduct({ userId, productId, size })
+                    const existingProduct = response?.documents?.[0]
+
+                    // Add new product if existing product is not available
+
+                    if (!existingProduct) {
+                        const addPayload = {
+                            userId: userId,
+                            ...product
+                        }
+                        const addedItem = await Service.addToCart(addPayload);
+                        return addedItem
                     }
-                    return Service.updateProduct(updatingItem)
-                    
-                }else {
-                    const itemdetails = {
-                        userId : userId,
-                        productId: item?.productId,
-                        price: item?.price,
-                        quantity: item?.quantity,
-                        size: item?.size,
-                    }
-                    const addedItem = await Service.addToCart(itemdetails);
-                    return addedItem
-                    
-                    
+
+                    // Updating product 
+                    const isSame = (product.quantity === existingProduct.quantity) && (product.size === existingProduct.size) && (userId === existingProduct.userId)
+                    //if everything is same ignor
+                    if (isSame) return null;
+
+                    return await Service.updateProduct(existingProduct.$id, product)
+                } catch (innerError) {
+                    console.error('Error syncing individual product:', product, innerError);
+                    return null;
                 }
             })
-        )
+            await Promise.all(syncTasks);
+            console.info('Cart successfully synced with database.');
+        } catch (error) {
+            console.error('Failed to sync cart:', error);
+            toast.error('Failed to sync cart with server.');
+            return rejectWithValue('Cart sync failed');
+        }
+
     }
 )
 
 export const deleteFromDb = createAsyncThunk(
     'cart/deleteFromDb',
-    async(documentId, {rejectWithValue}) =>{
+    async (documentId, { rejectWithValue }) => {
         try {
             await Service.removeFromCart(documentId);
             return documentId;
         } catch (error) {
             console.log("deleteFromAppwrite :: cartSlice :: error", error);
-            
+
             return rejectWithValue(error);
         }
 
@@ -81,16 +91,16 @@ export const deleteFromDb = createAsyncThunk(
 
 export const clearCartDb = createAsyncThunk(
     'cart/clearCartDb',
-    async(userId, {rejectWithValue}) =>{
+    async (userId, { rejectWithValue }) => {
         try {
             const cartItems = await Service.getAllCartProduct(userId);
-            Promise.all(cartItems.documents.map(async (item)=>{
-               await Service.removeFromCart(item.$id)
+            Promise.all(cartItems.documents.map(async (item) => {
+                await Service.removeFromCart(item.$id)
             }))
             return [];
         } catch (error) {
             console.log("clearCartFromAppwrite :: cartSlice :: error", error);
-            
+
             return rejectWithValue(error);
         }
     }
@@ -99,106 +109,108 @@ export const clearCartDb = createAsyncThunk(
 const cartSlice = createSlice({
     name: 'cart',
     initialState,
-    reducers:{
+    reducers: {
 
-        addToCart: (state, action)=>{
+        addToCart: (state, action) => {
             const newProduct = action.payload;
-            const existingProduct = state.products.find(product=>( product.productId === newProduct.productId) && (product.size === newProduct.size));
+            const existingProduct = state.products.find(product => (product.productId === newProduct.productId) && (product.size === newProduct.size));
             state.totalProducts++;
-            state.totalAmount = newProduct.price+state.totalAmount;
-            
+            state.totalAmount = newProduct.price + state.totalAmount;
 
-            if(!existingProduct){
-                state.products.push({ ...action.payload, quantity: 1, size: action.payload.size, price: action.payload.price});
-            }else{
+
+            if (!existingProduct) {
+                state.products.push({ ...action.payload, quantity: 1, size: action.payload.size, price: action.payload.price });
+            } else {
                 existingProduct.quantity++;
             }
         },
-   
 
-    removeFromCart: (state, action)=>{
-        const productId = action.payload.productId
-        const product = state.products.find(product=>product.productId === productId);
-        if (product) {
-            
-            state.products = state.products.filter(product=> !(product.productId === productId && product.size === action.payload.size));
-            
-            
-            state.totalAmount -= product.price*product.quantity;
-            state.totalProducts -= product.quantity;
+
+        removeFromCart: (state, action) => {
+            const productId = action.payload.productId
+            const product = state.products.find(product => product.productId === productId);
+            if (product) {
+
+                state.products = state.products.filter(product => !(product.productId === productId && product.size === action.payload.size));
+
+
+                state.totalAmount -= product.price * product.quantity;
+                state.totalProducts -= product.quantity;
+            }
+        },
+
+        incQty: (state, action) => {
+            const product = state.products.find(product => product.productId === action.payload.productId && product.size === action.payload.size);
+            if (product) {
+                product.quantity++;
+                state.totalAmount += product.price;
+                state.totalProducts++;
+            }
+        },
+
+        decQty: (state, action) => {
+            const product = state.products.find(product => product.productId === action.payload.productId && product.size === action.payload.size);
+            if (product && product.quantity > 1) {
+                product.quantity--;
+                state.totalAmount -= product.price;
+                state.totalProducts--;
+            }
+        },
+
+        emptyCart: (state) => {
+            state.products = [];
+            state.totalAmount = 0;
+            state.totalProducts = 0;
+        },
+
+        setCheckoutProducts: (state, action) => {
+            const { cartProducts, cakeMessage } = action.payload
+            const newProducts = cartProducts
+            newProducts?.forEach((newProduct) => {
+                const existingProduct = state.placeOrder?.find(product => (product.productId === newProduct.productId) && (product.size === newProduct.size));
+                if (existingProduct) {
+                    existingProduct.quantity++;
+                } else {
+                    state.placeOrder?.push({ ...newProduct, size: newProduct.size, });
+                    state.message = {
+
+                        ...state.message,
+                        ...cakeMessage
+
+                    }
+
+
+                }
+            })
+
         }
     },
-
-    incQty: (state, action)=>{
-        const product = state.products.find(product=>product.productId === action.payload.productId && product.size === action.payload.size);
-       if (product) {
-        product.quantity++;
-        state.totalAmount += product.price;
-        state.totalProducts++;
-       }
-    },
-
-    decQty: (state, action)=>{
-        const product = state.products.find(product=>product.productId === action.payload.productId && product.size === action.payload.size);
-       if (product && product.quantity > 1) {
-        product.quantity--;
-        state.totalAmount -= product.price;
-        state.totalProducts--;
-       }
-    },
-    
-    emptyCart: (state)=>{
-        state.products = [];
-        state.totalAmount = 0;
-        state.totalProducts = 0;
-    },
-
-    setCheckoutProducts: (state, action) => {
-        const { cartProducts, cakeMessage } = action.payload
-        const newProducts = cartProducts
-        newProducts?.forEach((newProduct)=>{const existingProduct = state.placeOrder?.find(product=>( product.productId === newProduct.productId) && (product.size === newProduct.size));
-        if (existingProduct) {
-            existingProduct.quantity++;
-        } else {
-            state.placeOrder?.push({ ...newProduct, size: newProduct.size,});
-            state.message = {
-                
-                    ...state.message,
-                   ...cakeMessage
-                
-            }
-            
-            
-        }})
-        
-      }
- },
- extraReducers: (builder) =>{
-    builder
-    .addCase(fetchCartFromDb.fulfilled, (state,action) =>{
-       action.payload.forEach((newProduct)=>{
-       const existingProduct = state.products.find((product)=>product.productId === newProduct.productId && product.size === newProduct.size);
-       if (existingProduct) {
-        existingProduct.quantity += newProduct.quantity
-       }else{
-        state.products.push(existingProduct)
-       }
-       state.totalProducts += newProduct.quantity;
-       state.totalAmount += newProduct.price * newProduct.quantity;
-        })
-       })
-    .addCase(deleteFromDb.fulfilled, (state,action)=>{
-        state.products = state.products.filter(product=>product.$id != action.payload);
-        state.totalProducts -= action.payload.quantity
-        state.totalAmount -+ action.payload.price*action.payload.quantity
-    })
-    .addCase(clearCartDb.fulfilled, (state)=>{
-        state.products=[];
-        state.totalProducts=0;
-        state.totalAmount=0;
-    });
- }
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchCartFromDb.fulfilled, (state, action) => {
+                action.payload.forEach((newProduct) => {
+                    const existingProduct = state.products.find((product) => product.productId === newProduct.productId && product.size === newProduct.size);
+                    if (existingProduct) {
+                        existingProduct.quantity += newProduct.quantity
+                    } else {
+                        state.products.push(existingProduct)
+                    }
+                    state.totalProducts += newProduct.quantity;
+                    state.totalAmount += newProduct.price * newProduct.quantity;
+                })
+            })
+            .addCase(deleteFromDb.fulfilled, (state, action) => {
+                state.products = state.products.filter(product => product.$id != action.payload);
+                state.totalProducts -= action.payload.quantity
+                state.totalAmount - + action.payload.price * action.payload.quantity
+            })
+            .addCase(clearCartDb.fulfilled, (state) => {
+                state.products = [];
+                state.totalProducts = 0;
+                state.totalAmount = 0;
+            });
+    }
 });
 
-export const {addToCart, removeFromCart, incQty, decQty, emptyCart, setCheckoutProducts} = cartSlice.actions;
+export const { addToCart, removeFromCart, incQty, decQty, emptyCart, setCheckoutProducts } = cartSlice.actions;
 export default cartSlice.reducer;
